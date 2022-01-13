46859cec20b236e546f8bcca68b559844c5ca71c
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2022-01-13 13:44:59 +0530

    MODIFIED RnaTranscription and RnaTranscriptionTest

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,22 +1,28 @@
 class RnaTranscription {
 
+    private final char adenine = 'A';
+    private final char cytosine = 'C';
+    private final char guanine = 'G';
+    private char thymine = 'T';
+    private char uracil = 'U';
+
     String transcribe(String dnaStrand) {
         int size = dnaStrand.length();
-        char arr[] = dnaStrand.toCharArray();
-        for (int i=0; i<size; i++)
+        char strandArray[] = dnaStrand.toCharArray();
+        for (int nucleotide=0; nucleotide<size; nucleotide++)
         {
-            if (arr[i]=='G')
-                arr[i] = 'C';
-            else if (arr[i]=='C')
-                arr[i] = 'G';
-            else if (arr[i]=='T')
-                arr[i] = 'A';
-            else if (arr[i]=='A')
-                arr[i] = 'U';
+            if (strandArray[nucleotide]==guanine)
+                strandArray[nucleotide] = cytosine;
+            else if (strandArray[nucleotide] ==cytosine)
+                strandArray[nucleotide] = guanine;
+            else if (strandArray[nucleotide] == thymine)
+                strandArray[nucleotide] = adenine;
+            else if (strandArray[nucleotide]== adenine)
+                strandArray[nucleotide] = uracil;
             else
                 continue;
         }
-        String rna_transcribed = new String(arr);
+        String rna_transcribed = new String(strandArray);
         return rna_transcribed;
     }
 
